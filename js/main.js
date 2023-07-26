6a12619066c11b5372d9b66c0b7e428337f429a5
bernardaargat <134631060+bernardaargat@users.noreply.github.com>
2023-07-25 22:47:56 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -134,3 +134,138 @@ function obtenerInformacionMueble(nombre, color, material, precio, descripcion) 
   // Ejemplo de uso de las funciones
   console.log(obtenerInformacionMueble("Silla", "Blanco", "Madera", 50, "Una silla de madera blanca elegante."));
   console.log(obtenerInformacionLampara("Lámpara de techo", "Plateado", "Metal", 80, "Una lámpara de techo moderna y elegante."));
+
+  //clase:5
+
+// Definición del objeto "mueble"
+var mueble = {
+  nombre: "Silla",
+  color: "Blanco",
+  material: "Madera",
+  precio: 50,
+  descripcion: "Una silla de madera blanca elegante."
+};
+
+// Acceso a las propiedades del objeto
+console.log(mueble.nombre); // "Silla"
+console.log(mueble.color); // "Blanco"
+console.log(mueble.material); // "Madera"
+console.log(mueble.precio); // 50
+console.log(mueble.descripcion); // "Una silla de madera blanca elegante."
+
+// Modificación de propiedades del objeto
+mueble.color = "Negro";
+console.log(mueble.color); // "Negro"
+
+// Definición de otro objeto "lampara"
+var lampara = {
+  nombre: "Lámpara de techo",
+  color: "Plateado",
+  material: "Metal",
+  precio: 80,
+  descripcion: "Una lámpara de techo moderna y elegante."
+};
+
+// Acceso a las propiedades del objeto "lampara"
+console.log(lampara.nombre); // "Lámpara de techo"
+console.log(lampara.color); // "Plateado"
+console.log(lampara.material); // "Metal"
+console.log(lampara.precio); // 80
+console.log(lampara.descripcion); // "Una lámpara de techo moderna y elegante."
+
+
+//clase:6
+
+// Array para los muebles
+var muebles = [
+  ["Silla", "Blanco", "Madera", 50, "Una silla de madera blanca elegante."],
+  ["Mesa", "Negro", "Acero", 120, "Una mesa de acero negra y resistente."],
+  ["Sofá", "Gris", "Cuero", 300, "Un sofá de cuero gris cómodo y elegante."]
+];
+
+// Array para las lámparas
+var lamparas = [
+  ["Lámpara de techo", "Plateado", "Metal", 80, "Una lámpara de techo moderna y elegante."],
+  ["Lámpara de pie", "Negro", "Hierro", 60, "Una lámpara de pie de hierro negro con pantalla ajustable."],
+  ["Lámpara de escritorio", "Blanco", "Plástico", 25, "Una lámpara de escritorio blanca con brazo flexible."]
+];
+
+// Función para obtener información de un artículo
+function obtenerInformacion(articulo) {
+  return "Nombre: " + articulo[0] + "\n" +
+         "Color: " + articulo[1] + "\n" +
+         "Material: " + articulo[2] + "\n" +
+         "Precio: $" + articulo[3] + "\n" +
+         "Descripción: " + articulo[4];
+}
+
+// Recorrer y mostrar los muebles
+console.log("Muebles:");
+for (var i = 0; i < muebles.length; i++) {
+  console.log(obtenerInformacion(muebles[i]));
+}
+
+// Recorrer y mostrar las lámparas
+console.log("Lámparas:");
+for (var j = 0; j < lamparas.length; j++) {
+  console.log(obtenerInformacion(lamparas[j]));
+}
+
+//clase:7
+
+// Array de muebles
+var muebles = [
+  { nombre: "Silla", color: "Blanco", material: "Madera", precio: 50, descripcion: "Una silla de madera blanca elegante." },
+  { nombre: "Mesa", color: "Negro", material: "Acero", precio: 120, descripcion: "Una mesa de acero negra y resistente." },
+  { nombre: "Sofá", color: "Gris", material: "Cuero", precio: 300, descripcion: "Un sofá de cuero gris cómodo y elegante." }
+];
+
+// Array de lámparas
+var lamparas = [
+  { nombre: "Lámpara de techo", color: "Plateado", material: "Metal", precio: 80, descripcion: "Una lámpara de techo moderna y elegante." },
+  { nombre: "Lámpara de pie", color: "Negro", material: "Hierro", precio: 60, descripcion: "Una lámpara de pie de hierro negro con pantalla ajustable." },
+  { nombre: "Lámpara de escritorio", color: "Blanco", material: "Plástico", precio: 25, descripcion: "Una lámpara de escritorio blanca con brazo flexible." }
+];
+
+// Función para obtener información de un artículo
+function obtenerInformacion(articulo) {
+  return "Nombre: " + articulo.nombre + "\n" +
+         "Color: " + articulo.color + "\n" +
+         "Material: " + articulo.material + "\n" +
+         "Precio: $" + articulo.precio + "\n" +
+         "Descripción: " + articulo.descripcion;
+}
+
+// Utilizar el método map para obtener información de los muebles
+console.log("Muebles:");
+muebles.map(function(articulo) {
+  console.log(obtenerInformacion(articulo));
+});
+
+// Utilizar el método forEach para obtener información de las lámparas
+console.log("Lámparas:");
+lamparas.forEach(function(articulo) {
+  console.log(obtenerInformacion(articulo));
+});
+
+
+//clase:8
+
+// Esperamos a que el DOM cargue completamente antes de ejecutar el código
+document.addEventListener('DOMContentLoaded', function () {
+// Acceder al header y crear un nuevo elemento "span"
+const header = document.querySelector('La casona GB');
+const newElement = document.createElement('span');
+newElement.textContent = '¡bienvenido!';
+header.appendChild(newElement);
+
+// Cambiar el texto del elemento con la clase "diseño"
+const diseñoElement = document.querySelector('.diseño');
+diseñoElement.innerHTML = '<h1>Nuevo título</h1><p>Nuevo texto</p>';
+
+// Ajustar el atributo "alt" de las imágenes
+const images = document.querySelectorAll('.img6');
+images.forEach((img, index) => {
+  img.alt = `Imagen ${index + 1}`;
+});
+});
